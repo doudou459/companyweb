3506d7b04b099bde8dda3f23935e4da3e4908a17
doudou459 <459349489@qq.com>
2019-11-13 23:43:12 +0800

    add resources

--- src/main/java/com/chaoweifish/companyweb/service/AdminLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.chaoweifish.companyweb.service;
-
-import java.util.HashMap;
-
-public interface AdminLoginService {
-    HashMap<String,Object> doLogin(String loginID, String loginKey);
-    String changeKey(String loginID,String oldKey,String newKey);
-}
